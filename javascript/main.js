15fa23b9567afcd7ccdaf80b4c8d6128655f7d2d
haim arka <haimarka1@gmail.com>
2021-08-21 22:33:00 +0300

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,74 @@
+//1
+
+// function returnNumber(){
+//     return Math.floor(Math.random()* 100);
+// }
+// console.log(returnNumber());
+
+//2
+
+// function returnNumber(){
+//     return Math.floor(Math.random()*10000)
+// }
+// console.log(returnNumber());
+
+//3
+
+// function returnNumber(){
+//     return Math.floor(Math.random()*55)
+// }
+// console.log(returnNumber());
+
+//4
+
+// var num = Number(prompt("enter number"));
+// function getNumberAndReturn(theNumber){
+//     return Math.floor(Math.random()* theNumber);
+// }
+// console.log(getNumberAndReturn(num));
+
+//5
+
+// var num = Number(prompt("enter number"));
+// function getNumberAndCheck(randumNumber){
+//     randumNumber = Math.floor(Math.random()* 10);
+//     if(num == randumNumber){
+//         console.log(randumNumber);
+//         return "even number";
+//     }
+//     console.log(randumNumber);
+//     return "not even number";
+    
+  
+// }
+// console.log(getNumberAndCheck(num));
+
+//6
+
+// var num = Number(prompt("enter number"));
+// function getNumberAndCheck(randumNumber){
+//     randumNumber = Math.floor(Math.random()* 100);
+//     if(num != randumNumber){
+//         console.log(randumNumber);
+//         return "not even number";
+//     }
+//     console.log(randumNumber);
+//     return "even number";
+    
+// }
+// console.log(getNumberAndCheck(num));
+
+//7
+
+// var firstName = (prompt("enter first name"));
+// var num = Number(prompt("enter number"));
+// function getNumberAndCheck(randumNumber){
+//     randumNumber = Math.floor(Math.random()* 4);
+//     if(num == randumNumber){
+//         console.log(randumNumber,firstName," you won"); 
+//         return  
+//     }
+//     console.log(randumNumber," try again");
+//     return 
+// }   
+// getNumberAndCheck(firstName,num);
